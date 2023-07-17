fdcca418727a54e890a87fb2adef78195723ebae
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-07-17 09:45:33 +0100

    commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,45 @@
-
-import NexthAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import { connectToDB  } from "@utils/database";
+import User from "@models/user"
 
-const handler = NexthAuth({
+const handler = NextAuth({
+
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email
+    })
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      // check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email
+      })
+
+      // if not, create a new user
+      if(!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace("", "").toLowerCase(),
+          image: profile.picture
+        })
+      }
+      return true;
+    } catch (error) {}
+  },
 });
+
+export { handler as GET, handler as POST };
